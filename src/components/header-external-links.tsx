26da893171fdb8e0e69c0e32215e5b5ff152f753
Jesiel Viana <jesiel.analista@gmail.com>
2021-01-29 08:59:03 -0300

    add _blank to external link

--- src/components/header-external-links.tsx
+++ src/components/header-external-links.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
-import React from "react"
-import { jsx, Link as TLink } from "theme-ui"
-import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
+import React from 'react'
+import { jsx, Link as TLink } from 'theme-ui'
+import useMinimalBlogConfig from '../hooks/use-minimal-blog-config'
 
 const HeaderExternalLinks = () => {
   const { externalLinks } = useMinimalBlogConfig()
@@ -9,9 +9,9 @@ const HeaderExternalLinks = () => {
   return (
     <React.Fragment>
       {externalLinks && externalLinks.length > 0 && (
-        <div sx={{ "a:not(:first-of-type)": { ml: 3 }, fontSize: [1, `18px`] }}>
-          {externalLinks.map((link) => (
-            <TLink key={link.url} href={link.url}>
+        <div sx={{ 'a:not(:first-of-type)': { ml: 3 }, fontSize: [1, `18px`] }}>
+          {externalLinks.map(link => (
+            <TLink target='_blank' key={link.url} href={link.url}>
               {link.name}
             </TLink>
           ))}
